f98eb21f9142dd503453f74a770d32f0a8b29eb6
João Pedro Lima <joaopedrocal90@gmail.com>
2025-10-30 12:56:26 -0300

    build: changed elixir version to default

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ToyRobot.MixProject do
     [
       app: :toy_robot,
       version: "0.1.0",
-      elixir: "~> 1.17.3",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
